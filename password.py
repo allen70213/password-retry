0f5fefcbc9771c3d9a206a3f9ddd383a72cf78e8
Allen Teng <allen70213@gmail.com>
2022-12-28 15:52:02 +0800

    update

--- password.py
+++ password.py
@@ -1,13 +1,16 @@
 password = 'a123456'
 chance = 3
-while True :
+while chance > 0 :
+    chance = chance - 1
     answer = input('請輸入密碼: ')
     if answer == password :
         print('登入成功')
         break
     else :
-        chance = chance - 1
-        print('密碼錯誤! 還有', chance, '次機會')
-        if chance == 0:
-            break
+        print('密碼錯誤!')
+        if chance > 0:
+            print('還有', chance, '次機會')
+        else:
+            print('你已錯3次密碼，沒機會嘗試了')
+
 
